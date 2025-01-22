5559d1ea0392b4535051bacec874cb299feaf672
Marina <climovicimarina@gmail.com>
2025-01-22 12:40:35 +0100

    db config; .env

--- business_expenses/business_expenses/settings.py
+++ business_expenses/business_expenses/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'business_expenses.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': os.getenv('DB_USER'),
+        'NAME': os.getenv('DB_NAME'),
+        'PORT': os.getenv('DB_PORT'),
+        'HOST': os.getenv('DB_HOST'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
     }
 }
 
