04abca115cc648852163897e4fa5738183cdfd23
Marina <climovicimarina@gmail.com>
2025-01-26 14:30:43 +0100

    log_in

--- business_expenses/app/urls.py
+++ business_expenses/app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('sign_up/', views.sign_up, name="sign_up"),
+    path('signup/', views.signup, name="signup"),
     path('home/', views.home, name='home'),
-]
+    path('log_in/', views.log_in, name='log_in')
+    ]
--- business_expenses/app/views.py
+++ business_expenses/app/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import AuthenticationForm
 from .forms import UserRegistration
 
 # Create your views here.
-def sign_up(request):
+def signup(request):
     if request.method == "POST":
         registration_form = UserRegistration(request.POST)
         if registration_form.is_valid():
@@ -13,16 +14,27 @@ def sign_up(request):
     else:
         registration_form = UserRegistration()
 
-    return render(request, 'sign_up.html', {'registration_form': registration_form})
+    return render(request, 'signup.html', {'registration_form': registration_form})
 
 def log_in(request):
-    raise NotImplementedError("TO DO")
+    if request.method == "POST":
+        login_form = AuthenticationForm(data=request.POST)
+
+        if login_form.is_valid():
+            user = login_form.get_user()
+            login(request, user)
+            return redirect("/home")
+    else:
+        login_form = AuthenticationForm()
+
+    return render(request, "log_in.html", {"login_form": login_form})
+
 
 def logout(request):
     raise NotImplementedError("TO DO")
 
 def home(request):
-    raise NotImplementedError("TO DO")
+    return render(request, 'home.html')
 
 def daily_summary(request):
     raise NotImplementedError("TO DO")
