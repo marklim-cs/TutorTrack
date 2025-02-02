69297dd46530e907af84be7d13e285ba3af52600
Marina <climovicimarina@gmail.com>
2025-02-02 15:35:30 +0100

    models refining

--- tutor_track/app/admin.py
+++ tutor_track/app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Student, Language, Rate, Lesson
+from .models import Student, Language, Lesson, MonthlyRecord, Day
 
 # Register your models here.
 class StudentAdmin(admin.ModelAdmin):
@@ -8,13 +8,21 @@ class StudentAdmin(admin.ModelAdmin):
 class LanguageAdmin(admin.ModelAdmin):
     list_display = ("id", "name")
 
-class RateAdmin(admin.ModelAdmin):
-    list_display = ("id", "student", "rate", "language", "tutor")
-
 class LessonAdmin(admin.ModelAdmin):
-    list_display = ("id", "rate", "date", "time")
+    def get_days(self, obj):
+        return ", ".join([day.day for day in obj.day.all()])
+    get_days.short_description = 'Days'
+
+    list_display = ("id", "student", "get_days", "rate", "language", "tutor")
+
+class MonthlyRecordAdmin(admin.ModelAdmin):
+    list_display = ("id", "lesson", "count", "total_money")
+
+class DayAdmin(admin.ModelAdmin):
+    list_display = ("id", "day")
 
 admin.site.register(Student, StudentAdmin)
 admin.site.register(Language, LanguageAdmin)
-admin.site.register(Rate, RateAdmin)
 admin.site.register(Lesson, LessonAdmin)
+admin.site.register(MonthlyRecord, MonthlyRecordAdmin)
+admin.site.register(Day, DayAdmin)
--- tutor_track/app/models.py
+++ tutor_track/app/models.py
@@ -8,18 +8,33 @@ class Student(models.Model):
     last_name = models.CharField(max_length=250, null=True)
 
     def __str__(self):
-        return f"{self.first_name}"
+        return f"{self.first_name} {self.last_name}"
 
 class Language(models.Model):
-    name = models.CharField(max_length=250, blank=False)
+    name = models.CharField(max_length=250, blank=True, null=True)
 
-class Rate(models.Model):
-    student = models.ForeignKey(Student, on_delete=models.CASCADE)
-    rate = models.IntegerField(blank=False)
-    language = models.ForeignKey(Language, on_delete=models.CASCADE)
-    tutor = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return f"{self.name}"
+
+class Day(models.Model):
+    day = models.CharField(max_length=250, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.day}"
 
 class Lesson(models.Model):
-    rate = models.ForeignKey(Rate, on_delete=models.CASCADE)
-    date = models.DateField(auto_now=True)
-    time = models.TimeField(auto_now=True)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    rate = models.IntegerField(blank=True, null=True)
+    day = models.ManyToManyField(Day)
+    language = models.ForeignKey(Language, on_delete=models.CASCADE)
+    tutor = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return f"{self.student}, {self.rate}, {self.day}, {self.language}"
+
+class MonthlyRecord(models.Model):
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    count = models.IntegerField(blank=False)
+    total_money = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.lesson}, {self.count}, {self.total_money}"
